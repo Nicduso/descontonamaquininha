6bb06bb667252816ac01525570e0e8ebab3adee0
Nicduso <nicole.duarte1010@gmail.com>
2025-08-04 22:01:37 -0300

    Configuração de Banco de Dados AWS

--- public/php/view/test.php
+++ public/php/view/test.php
@@ -1,10 +1,12 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 
-    require_once("../model/Connection.class.php");
-    if(Connection::getInstance()) {
-        echo "Banco de Dados Conectado!";
-    } else {
-        echo "Erro ao conectar o Banco de Dados.";
-    }
-
+	require_once("../model/Connection.class.php");
+	if(Connection::getInstance()) {
+			echo "Banco de Dados Conectado!";
+	} else {
+			echo "Erro ao conectar o Banco de Dados.";
+	}
 ?>
