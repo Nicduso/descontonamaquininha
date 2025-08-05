b9bb5c7855fbd2f36ac1273766d47a6026323d01
Nicduso <nicole.duarte1010@gmail.com>
2025-08-04 22:50:05 -0300

    Configuração de Banco de Dados AWS

--- public/php/model/Connection.class.php
+++ public/php/model/Connection.class.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use Aws\Ssm\SsmClient;
+use Aws\Exception\AwsException;
 
 class Connection {
 	public static $instance;
@@ -9,27 +10,35 @@ class Connection {
 	public static function getInstance() {
 		if (!isset(self::$instance)) {
 			try {
-				$ssm = new SsmClient([
-					'version'     => 'latest',
-					'region'      => 'us-east-1',
-					'credentials' => [
-						'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-						'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-					],
+				$client = new SsmClient([
+					'version' => 'latest',
+					'region'  => 'us-east-1',
 				]);
+				$parameters = $client->getParametersByPath([
+					'Path' => '/descontonamaquininha/',
+					'WithDecryption' => true,
+				])['Parameters'];
 
-				$host   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_HOST', 'WithDecryption' => true])['Parameter']['Value'];
-				$user   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_USER', 'WithDecryption' => true])['Parameter']['Value'];
-				$pass   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_PASS', 'WithDecryption' => true])['Parameter']['Value'];
-				$dbname = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_NAME', 'WithDecryption' => true])['Parameter']['Value'];
+				$data = [];
+				foreach ($parameters as $param) {
+					$name = basename($param['Name']);
+					$data[$name] = $param['Value'];
+				}
 
-				self::$instance = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+				$host   = $data['DB_HOST'];
+				$user   = $data['DB_USER'];
+				$pass   = $data['DB_PASS'];
+				$dbname = $data['DB_NAME'];
+
+				$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+				self::$instance = new PDO($dsn, $user, $pass);
 				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			} catch(Exception $e) {
+
+			} catch (AwsException $e) {
 				echo "Erro ao conectar o Banco de Dados (AWS): " . $e->getMessage();
 			}
-		}
-		return self::$instance;
-	}
+    }
+    return self::$instance;
+  }
 }
 ?>
