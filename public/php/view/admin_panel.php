93ee9611f0d37b6095f8e0802d5293ead8790ad8
Nicduso <nicole.duarte1010@gmail.com>
2025-08-05 20:25:13 -0300

    Preparação de painel para acesso administrativo.

---
+++ public/php/view/admin_panel.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="../../images/favicon.png" type="image/x-icon">
+	<title>Painel Admin</title>
+	<link rel="stylesheet" href="../../css/reset.css">
+	<link rel="stylesheet" href="../../css/admin_panel.css">
+	<link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<img class="image-logo" src="../../images/logo-mobile-extends.svg" alt="Logo desconto na maquininha">
+	</header>
+	<main>
+		<div class="panel">
+			<form action="" class="panel-form">
+				<h1 class="form-title">Painel Administrativo</h1>
+				<div class="form-field"><input type="text" placeholder="Usuário"></div>
+				<div class="form-field"><input type="text" placeholder="Senha"></div>
+				<input type="submit" class="form-submit" value="Acessar">
+			</form>
+			<button class="pass-button">Alterar senha</button>
+		</div>
+	</main>
+</body>
+</html>
---
+++ public/php/view/product_registration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="../../images/favicon.png" type="image/x-icon">
+	<title>Manutenção de registros</title>
+	<link rel="stylesheet" href="../../css/reset.css">
+	<link rel="stylesheet" href="../../css/product_registration.css">
+	<link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap" rel="stylesheet">
+</head>
+<body>
+
+</body>
+</html>
