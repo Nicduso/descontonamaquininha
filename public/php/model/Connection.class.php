2ae36f7292ad5ca2efe9f3a6446e8ddbbc1c73c3
Nicduso <nicole.duarte1010@gmail.com>
2025-08-04 22:23:15 -0300

    Configuração de Banco de Dados AWS

--- public/php/model/Connection.class.php
+++ public/php/model/Connection.class.php
@@ -9,38 +9,24 @@ class Connection {
 	public static function getInstance() {
 		if (!isset(self::$instance)) {
 			try {
-				$environment = $_ENV['APP_ENV'] ?? 'local';
+				$ssm = new SsmClient([
+					'version'     => 'latest',
+					'region'      => 'us-east-1',
+					'credentials' => [
+						'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
+						'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+					],
+				]);
 
-				if ($environment === 'local') {
-					// Carrega Dotenv apenas no ambiente local
-					$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
-					$dotenv->load();
-
-					$host = $_ENV['DB_HOST'];
-					$dbname = $_ENV['DB_NAME'];
-					$user = $_ENV['DB_USER'];
-					$pass = $_ENV['DB_PASS'];
-				} else {
-					// Usa AWS Parameter Store
-					$ssm = new SsmClient([
-						'version'     => 'latest',
-						'region'      => 'us-east-1',
-						'credentials' => [
-							'key'    => $_ENV['AWS_ACCESS_KEY_ID'],
-							'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-						],
-					]);
-
-					$host   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_HOST', 'WithDecryption' => true])['Parameter']['Value'];
-					$user   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_USER', 'WithDecryption' => true])['Parameter']['Value'];
-					$pass   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_PASS', 'WithDecryption' => true])['Parameter']['Value'];
-					$dbname = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_NAME', 'WithDecryption' => true])['Parameter']['Value'];
-				}
+				$host   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_HOST', 'WithDecryption' => true])['Parameter']['Value'];
+				$user   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_USER', 'WithDecryption' => true])['Parameter']['Value'];
+				$pass   = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_PASS', 'WithDecryption' => true])['Parameter']['Value'];
+				$dbname = $ssm->getParameter(['Name' => '/descontonamaquininha/DB_NAME', 'WithDecryption' => true])['Parameter']['Value'];
 
 				self::$instance = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch(Exception $e) {
-				echo "Erro ao conectar o Banco de Dados: " . $e->getMessage();
+				echo "Erro ao conectar o Banco de Dados (AWS): " . $e->getMessage();
 			}
 		}
 		return self::$instance;
