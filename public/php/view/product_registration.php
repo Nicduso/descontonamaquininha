58f1d80c9c3eb6127bd25aa96aabf00ce1381901
Nicduso <nicole.duarte1010@gmail.com>
2025-08-14 13:57:56 -0300

    Atualização página 'product_registration'

--- public/php/view/product_registration.php
+++ public/php/view/product_registration.php
@@ -30,22 +30,24 @@
 						<th class="id-title">ID</th>
 						<th class="table-title">Operadora</th>
 						<th class="table-title">Maquininha</th>
+						<th class="table-title">Ação</th>
 					</tr>
 					<tr class="content-row">
 						<td class="table-content">0000</td>
 						<td class="table-content">Mercado Pago</td>
 						<td class="table-content">Point Mini</td>
+						<td class="table-content"><a href=""><i class="material-icons delete-icon">delete</i></a></td>
 					</tr>
 					<tr class="content-row">
 						<td class="table-content">0000</td>
 						<td class="table-content">Mercado Pago</td>
 						<td class="table-content">Point Mini</td>
+						<td class="table-content"><a href=""><i class="material-icons delete-icon">delete</i></a></td>
 					</tr>
 				</table>
 			</div>
 		</div>
 		<div class="product-container">
-			<div class="products-register">
 				<h1 class="form-title">Cadastro de Produto:</h1>
 				<form class="register-form">
 					<div class="form-line">
@@ -74,16 +76,16 @@
 						<label class="label-form">Imagem:</label>
 					<div class="form-upload upload-container">
 						<label for="file-upload" class="custom-upload"><i class="material-icons">upload</i>Escolher imagem</label>
-						<input type="file" id="file-upload" class="form-file">
+						<input type="file" id="file-upload" class="form-file" onchange="document.getElementById('file-name').textContent = this.files[0]?.name || 'Nenhum ficheiro selecionado'">
+  					<span id="file-name" class="file-name-custom">Nenhum ficheiro selecionado</span>
 					</div>
 					</div>
 					<div class="form-actions">
 						<input type="submit" value="Cadastrar" class="form-button">
 						<input type="submit" value="Alterar" class="form-button extra-button">
-						<input type="submit" value="Excluir" class="form-button extra-button">
+						<input type="submit" value="Limpar campos" class="form-button extra-button">
 					</div>
 				</form>
-			</div>
 			<p class="info-text">Há mais funções acessíveis nas resoluções de desktop.</p>
 		</div>
 	</main>
